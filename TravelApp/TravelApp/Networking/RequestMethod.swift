de57aebefb7d2805927dcee0239ff2f76c9ca591
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-01-21 19:38:33 -0300

    Add RequestMethod

---
+++ TravelApp/TravelApp/Networking/RequestMethod.swift
@@ -0,0 +1,15 @@
+//
+//  RequestMethod.swift
+//  TravelApp
+//
+//  Created by Barbara Souza on 21/01/21.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case post = "POST"
+    case patch = "PATCH"
+    case get = "GET"
+    case put = "PUT"
+}
