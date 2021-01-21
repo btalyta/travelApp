ff4d2c52bbc28f6e9ebd0dfbbd646974f69abfb3
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-01-21 20:38:28 -0300

    Add models

---
+++ TravelApp/TravelApp/Networking/Model/Connection.swift
@@ -0,0 +1,25 @@
+//
+//  RouteModel.swift
+//  TravelApp
+//
+//  Created by Barbara Souza on 21/01/21.
+//
+
+import Foundation
+
+struct Connection: Decodable, Equatable {
+    let from: String
+    let to: String
+    let price: Double
+    let coordinates: Coordinates
+}
+
+struct Coordinates: Decodable, Equatable {
+    let from: Location
+    let to: Location
+}
+
+struct Location: Decodable, Equatable {
+    let lat: Double
+    let long: Double
+}
