913cc93f56a7735bb3b7eeeb26fe9ac7b81e6491
Bárbara Souza <b.barbaratallyta@gmail.com>
2021-01-23 23:10:45 -0300

    Add search protocols

--- TravelApp/TravelApp/SearchView/SearchProtocols.swift
+++ TravelApp/TravelApp/SearchView/SearchProtocols.swift
@@ -10,3 +10,23 @@ import Foundation
 protocol SearchRepositoryProtocols {
     func requestConnections(completion: @escaping ((Result<[Connection], APIError>) -> Void))
 }
+
+protocol SearchPresenterProtocol {
+    var viewController: SearchViewControllerProtocol? { get set }
+    func load()
+    func didSelectDepartureInput()
+    func didSelectArrivalInput()
+    func didTapSuggestion(_ city: City)
+    func didChangeInput(_ city: String)
+    func wantsSearch()
+}
+
+protocol SearchViewControllerProtocol: class {
+    func show(_ viewModel: SearchViewModel)
+    func showError(message: String, retry: Bool)
+    func wantsShowFlight(_ flight: [Trip])
+}
+
+protocol SearchViewControllerDelegate: class {
+    func wantsShowFlight(_ flight: [Trip])
+}
